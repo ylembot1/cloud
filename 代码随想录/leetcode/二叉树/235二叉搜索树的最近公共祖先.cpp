57bb0b4e65ac824768639686f8acea45eb6e84b8
lofee <1143385098@qq.com>
2024-04-20 00:13:08 +0800

    hh

---
+++ 代码随想录/leetcode/二叉树/235二叉搜索树的最近公共祖先.cpp
@@ -0,0 +1,23 @@
+#include "../header.h"
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root == nullptr) {
+            return root;
+        }
+
+        if (root->val > p->val && root->val < q->val || root->val < p->val && root->val > q->val) {
+            return root;
+        }
+
+        if (root == p || root == q) {
+            return root;
+        }
+
+        if (root->val > p->val) {
+            return lowestCommonAncestor(root->left, p, q);
+        }
+        return lowestCommonAncestor(root->right, p, q);
+    }
+};
